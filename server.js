590c0f74774f262334fdcfa3d93945e927160f56
rogerrg2009-source <rogerrg2009@gmail.com>
2026-08-11 18:47:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 10000;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "chicoia_bar";
-
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;
 // Teste do servidor
 app.get("/", (req, res) => {
   res.send("ChicoIA WhatsApp está funcionando!");
@@ -37,9 +38,31 @@ console.log(JSON.stringify(body, null, 2));
       const message =
         body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
 
-      if (message) {
-        console.log("Mensagem recebida:", message);
+    if (message) {
+  console.log("Mensagem recebida:", message);
+
+  const numeroCliente = message.from;
+
+  await axios.post(
+    `https://graph.facebook.com/v26.0/${PHONE_NUMBER_ID}/messages`,
+    {
+      messaging_product: "whatsapp",
+      to: numeroCliente,
+      type: "text",
+      text: {
+        body: "Olá! 👋 Eu sou o ChicoIA, assistente virtual do Bar do Seu Chico. Como posso ajudar?"
       }
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${WHATSAPP_TOKEN}`,
+        "Content-Type": "application/json"
+      }
+    }
+  );
+
+  console.log("Resposta enviada para:", numeroCliente);
+}
     }
 
     res.sendStatus(200);
