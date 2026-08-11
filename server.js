59bf2aa044d0022e77d040e7d6fb60533e04c79e
rogerrg2009-source <rogerrg2009@gmail.com>
2026-08-11 20:27:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -66,13 +66,13 @@ console.log(JSON.stringify(body, null, 2));
     }
 
     res.sendStatus(200);
- } catch (error) {
-  console.error(
-    "ERRO WHATSAPP:",
-    JSON.stringify(error.response?.data || error.message, null, 2)
-  );
-  res.sendStatus(500);
-}
+  } catch (error) {
+    console.error(
+      "ERRO WHATSAPP:",
+      JSON.stringify(error.response?.data || error.message, null, 2)
+    );
+
+    res.sendStatus(500);
   }
 });
 
